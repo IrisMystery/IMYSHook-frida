60de69e33790535eed1cb2d4b41ef4af888f1f61
dewrf <dewrfe53535@gmail.com>
2025-01-16 16:32:19 +0800

    more debug info

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,45 @@
+function logInAndroid(log) {
+    Java.perform(() => {
+        let Log = Java.use("android.util.Log");
+        let TAG_L = "[imyshook-frida]";
+        Log.v(TAG_L, log);
+    });
+}
+
+(function hijackConsole() {
+    let originalLog = console.log;
+    let originalError = console.error;
+
+    console.log = function (...args) {
+        let message = args.map(arg => {
+            if (typeof arg === "object") {
+                return JSON.stringify(arg, null, 2);
+            }
+            return String(arg);
+        }).join(" ");
+
+        logInAndroid(message);
+
+        originalLog.apply(console, args);
+    };
+
+    console.error = function (...args) {
+        let message = args.map(arg => {
+            if (typeof arg === "object") {
+                return JSON.stringify(arg, null, 2);
+            }
+            return String(arg);
+        }).join(" ");
+
+        logInAndroid(`[ERROR] ${message}`);
+        originalError.apply(console, args);
+    };
+})();
+
+import "frida-il2cpp-bridge";
+
+Il2Cpp.installExceptionListener("all")
+
 import "./gameClass.js";
 import "./patch.js";
+
